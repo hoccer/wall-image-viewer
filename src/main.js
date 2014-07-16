e0c740361d946c3dc69fe42ef7288ff347d7fcb7
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-16 16:58:26 +0200

    Update model with incoming WebSocket messages

--- src/main.js
+++ src/main.js
@@ -23,6 +23,28 @@ jQuery.ajaxPrefilter(function(options, originalOptions, jqXHR) {
 var images = new DownloadCollection();
 images.fetch({data: {mediaType: 'image'}});
 
+// ---8<-----------------------------------------------------------------------
+
+var updatesUrl = Config.BACKEND_URL.replace('http', 'ws') + '/updates';
+var socket = new WebSocket(updatesUrl);
+
+socket.onopen = function() {
+  socket.send(JSON.stringify({
+    command: 'subscribe',
+    path: '/api/downloads'
+  }));
+};
+
+socket.onmessage = function(event) {
+  var object = JSON.parse(event.data);
+
+  if (object.hasOwnProperty('clientDownloadId')) {
+    images.add(object, {merge: true});
+  }
+};
+
+// ---8<-----------------------------------------------------------------------
+
 React.renderComponent(
   /* jshint ignore:start */
   <ImageGridView collection={images} />,
