865640741f2584b6375b043c40f6573980588636
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-16 16:22:26 +0200

    Add missing idAttribute field

--- src/models/download-model.js
+++ src/models/download-model.js
@@ -5,6 +5,8 @@ var Backbone = require('backbone');
 var Config = require('../config');
 
 module.exports = Backbone.Model.extend({
+  idAttribute: 'clientDownloadId',
+
   fileUrl: function() {
     return Config.BACKEND_URL + "/" + this.get('dataFile');
   }
