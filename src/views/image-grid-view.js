0e61397aafb4b79fb4d15e00637fc257beeb867e
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-16 09:00:42 +0200

    List returned downloads in image grid view

--- src/views/image-grid-view.js
+++ src/views/image-grid-view.js
@@ -7,10 +7,14 @@ var BackboneReactComponent = require('backbone-react-component');
 
 module.exports = React.createClass({
   mixins: [BackboneReactComponent.mixin],
-
+  renderImage: function(image) {
+    /* jshint ignore:start */
+    return <div key={image.id}>{image.get('dataFile')}</div>;
+    /* jshint ignore:end */
+  },
   render: function() {
     /* jshint ignore:start */
-    return <div>Number of Downloads: {this.props.collection.length}</div>;
+    return <div>{this.getCollection().map(this.renderImage)}</div>;
     /* jshint ignore:end */
   }
 });
