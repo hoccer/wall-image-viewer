d7117e770d996064da2173e44e45dc8769b35f04
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-16 09:02:21 +0200

    Add some newlines

--- src/views/image-grid-view.js
+++ src/views/image-grid-view.js
@@ -7,11 +7,13 @@ var BackboneReactComponent = require('backbone-react-component');
 
 module.exports = React.createClass({
   mixins: [BackboneReactComponent.mixin],
+
   renderImage: function(image) {
     /* jshint ignore:start */
     return <div key={image.id}>{image.get('dataFile')}</div>;
     /* jshint ignore:end */
   },
+
   render: function() {
     /* jshint ignore:start */
     return <div>{this.getCollection().map(this.renderImage)}</div>;
