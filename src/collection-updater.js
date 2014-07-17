07dcabe20b2224875d0e26afb8f924c406e5dda4
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-17 12:18:46 +0200

    Reconnect to WebSocket when connection is lost

--- src/collection-updater.js
+++ src/collection-updater.js
@@ -1,17 +1,52 @@
 'use strict';
 
 var _ = require('underscore');
+var Promise = require('promise');
+
+var RESTART_TIMEOUT = 10;
 
 var CollectionUpdater = module.exports = function(url) {
+  this.connectPromise = null;
   this.socket = null;
   this.subscriptions = {};
   this.url = url;
 };
 
 CollectionUpdater.prototype.start = function() {
-  this.socket = new WebSocket(this.url);
-  this.socket.onopen = this._onopen.bind(this);
-  this.socket.onmessage = this._onmessage.bind(this);
+  var _this = this;
+
+  var promise = new Promise(function(resolve, reject) {
+    if (_this._connection) {
+      resolve();
+    } else if (_this.connectPromise) {
+      return _this.connectPromise.promise;
+    } else {
+      _this.connectPromise = {
+        promise: promise,
+        resolve: resolve,
+        reject: reject
+      };
+
+      _this.socket = _this._openWebSocket(_this.url);
+    }
+  });
+
+  return promise;
+};
+
+CollectionUpdater.prototype._openWebSocket = function(url) {
+  var socket = null;
+
+  try {
+    socket = new WebSocket(url);
+    socket.onopen = this._onopen.bind(this);
+    socket.onmessage = this._onmessage.bind(this);
+    socket.onclose = this._onclose.bind(this);
+  } catch (exception) {
+    // returning null socket
+  }
+
+  return socket;
 };
 
 CollectionUpdater.prototype.subscribe = function(path, collection) {
@@ -34,6 +69,9 @@ CollectionUpdater.prototype._onopen = function() {
   _.each(_.keys(this.subscriptions), function(path) {
     _.each(this.subscriptions[path], this._doSubscribe.bind(this, path));
   }, this);
+
+  this.connectPromise.resolve();
+  this.connectPromise = null;
 };
 
 CollectionUpdater.prototype._onmessage = function(event) {
@@ -42,6 +80,28 @@ CollectionUpdater.prototype._onmessage = function(event) {
   _.each(this.subscriptions[message.path], function(collection) {
     collection.add(message.data, {at: 0, merge: true});
   });
+};
+
+CollectionUpdater.prototype._onclose = function() {
+  if (this.connectPromise) {
+    this.connectPromise.reject();
+    this.connectPromise = null;
+  }
+
+  this._scheduleRestart();
+};
+
+CollectionUpdater.prototype._scheduleRestart = function() {
+  console.warn(
+    'No connection to ' + this.url +
+    ', retrying in ' + RESTART_TIMEOUT + ' seconds.'
+  );
+
+  var _this = this;
+
+  setTimeout(function () {
+    _this.start().catch(_this._scheduleRestart.bind(_this));
+  }, RESTART_TIMEOUT * 1000);
 };
 
 CollectionUpdater.prototype._send = function(payload) {
