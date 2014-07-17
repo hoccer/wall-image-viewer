96064e32f34b4b0993d77a835287b1bac141cde0
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-17 14:23:59 +0200

    Add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+  return browserify('./src/main.js')
+    .transform(reactify)
+    .bundle({ debug: true })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['browserify']);
