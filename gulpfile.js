ef2d186b6f3151e888319f7a1df79eac8b681cce
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-18 08:14:09 +0200

    Add LESS styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,25 @@
 var browserify = require('browserify');
 var connect = require('gulp-connect');
 var gulp = require('gulp');
+var less = require('gulp-less');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('browserify', function() {
   return browserify('./js/main.js')
     .transform(reactify)
     .bundle({ debug: true })
     .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('less', function() {
+  gulp.src('./less/main.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'))
     .pipe(connect.reload());
 });
@@ -23,6 +34,7 @@ gulp.task('server', function() {
 
 gulp.task('watch', ['server'], function() {
   gulp.watch('./js/**/*.js', ['browserify']);
+  gulp.watch('./less/**/*.less', ['less']);
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('default', ['browserify', 'less']);
