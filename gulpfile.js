2c9f988d6f59d1538a32cce919c7492c9237f82a
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-31 12:04:47 +0200

    Minify JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,24 @@ var browserify = require('browserify');
 var connect = require('gulp-connect');
 var gulp = require('gulp');
 var less = require('gulp-less');
+var path = require('path');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 
+var distFolder = './dist';
+
 gulp.task('browserify', function() {
+  var sourceMap = 'bundle.map.json';
+
   return browserify({debug: true})
     .add('./js/main.js')
+    .plugin('minifyify', {
+      output: path.join(distFolder, sourceMap),
+      map: sourceMap
+    })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(distFolder))
     .pipe(connect.reload());
 });
 
@@ -21,7 +30,7 @@ gulp.task('less', function() {
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(distFolder))
     .pipe(connect.reload());
 });
 
